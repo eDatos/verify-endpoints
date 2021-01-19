58a48fcbac56e939e29e8b9048a4e388fe11fe9a
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-01-19 09:49:35 +0000

    Add verification of indicators

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+
 from istacpy.indicators.lite import indicators
+
+import config
 
 
 def verify_indicators():
+    failing_entrypoints = []
     all_indicators = indicators.get_indicators()
     for indicator_code, _ in all_indicators:
         indicator = indicators.get_indicator(indicator_code)
         try:
             indicator.get_data()
-        except Exception as err:
-            print(f'{indicator_code} is not available ({err})')
-            break
-
-
-if __name__ == "__main__":
-    verify_indicators()
+        except Exception:
+            failing_entrypoints.append(
+                indicator.api_response.get('selfLink', indicator_code)
+            )
+    return failing_entrypoints
