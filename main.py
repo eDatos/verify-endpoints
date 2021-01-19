35a0841c854117d04628fe9a13b3a2652a28989e
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-01-19 09:50:55 +0000

    Add notifications through email

--- main.py
+++ main.py
@@ -18,3 +18,34 @@ def verify_indicators():
                 indicator.api_response.get('selfLink', indicator_code)
             )
     return failing_entrypoints
+
+
+def notify(failing_entrypoints):
+    failing_entrypoints = '\n'.join(failing_entrypoints)
+    subject = '⚠️ Aviso sobre API entrypoints'
+    content = f'''Hola,
+
+Los siguientes API entrypoints parecen estar dando problemas:
+
+{failing_entrypoints}
+
+Saludos,
+El equipo de informática.
+'''
+
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg['Subject'] = subject
+    msg['From'] = config.NOTIFICATION_FROM_ADDR
+    msg['To'] = config.NOTIFICATION_TO_ADDRS
+
+    s = smtplib.SMTP(config.SMTP_SERVER, port=config.SMTP_PORT)
+    s.login(config.SMTP_USERNAME, config.SMTP_PASSWORD)
+    s.send_message(msg)
+    s.quit()
+
+
+if __name__ == "__main__":
+    failing_entrypoints = verify_indicators()
+    if failing_entrypoints:
+        notify(failing_entrypoints)
